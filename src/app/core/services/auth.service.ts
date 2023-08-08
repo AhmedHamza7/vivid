0373c212dd77ea62a42ef911d4c527b2251144e9
Ahmed Hamza <ahmd7mza@gmail.com>
2023-08-09 01:39:15 +0300

    - Finish UI of Login and Register . - Finish routing of auth.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  register(registerData:FormData):Observable<any> {
+    return this.http.post('https://tarmeezacademy.com/api/v1/register', registerData )
+  }
 }
--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../../../core/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  registerForm: FormGroup =  new FormGroup({})
+  constructor(
+    private AuthService:AuthService,
+    private fb:FormBuilder
+    ){}
 
-}
+    ngOnInit(): void {
+      this.initRegisterForm();
+      console.log(this.registerForm.controls);
+      
+    }
+    initRegisterForm(): void {
+      this.registerForm = this.fb.group({
+        username : ['', Validators.required],
+        password : ['', Validators.required],
+        name : ['', Validators.required],
+        email : ['', Validators.required],
+      })
+    }
+  // signUp() {
+  //   this.AuthService.register()
+  // }
+ }
