c246a51defb6f2d9cb8ef86d134cf4ea1d4ae32a
AhmedHamza7 <ahmd7mza@gmail.com>
2023-08-13 17:57:19 +0200

    - translate service start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './core/components/side-menu/side-menu.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +26,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    
+    ToastrModule.forRoot(), // ToastrModule added
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,18 +1,23 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './../../../core/services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonService } from 'src/app/core/services/common.service';
 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit{
   registerForm: FormGroup =  new FormGroup({})
+  RegisterUserData:any
+  userToken:any
   constructor(
     private AuthService:AuthService,
-    private fb:FormBuilder
+    private fb:FormBuilder,
+    private commonService:CommonService
     ){}
 
     ngOnInit(): void {
@@ -23,7 +28,7 @@ export class RegisterComponent implements OnInit{
         username : ['', Validators.required],
         password : ['', Validators.required],
         name : ['', Validators.required],
-        email : ['', Validators.required],
+        email : ['', [Validators.required, Validators.email]],
       })
     }
     get registerFormControl() {
@@ -32,7 +37,15 @@ export class RegisterComponent implements OnInit{
 
     signUp() {
       this.AuthService.register(this.registerForm.value).subscribe({
-        next: (res)=> console.log(res)
+        next: (res)=> {
+          console.log(res);
+          this.RegisterUserData = res.user
+          this.userToken = res.token
+          this.commonService.fireSuccessToastr("successfully registered", 'Success')
+        },
+        error: (err) => {
+          this.commonService.fireErrorToastr(err.error.message, 'Error')
+        }
       })
     }
- }
+}
